609470039dbbc2d023a424f40d0574b75f9054ab
VoltageMC <62860563+VoltageMC@users.noreply.github.com>
2020-04-06 18:45:35 -0400

    Add files via upload

---
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/CustomEnchantIds.php
@@ -0,0 +1,147 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaPigGuy\PiggyCustomEnchants\enchants;
+
+class CustomEnchantIds
+{
+    /**
+     * Global
+     */
+    const AUTOREPAIR = 108;
+    const SOULBOUND = 118;
+
+    /**
+     * Weapons
+     */
+    const LIFESTEAL = 200;
+    const BLIND = 201;
+    const VIPER = 202;
+    const CONFUSION = 204;
+    const EXECUTE = 205;
+    const CANABILISM = 206;
+    const WITHER = 207;
+    const SWORDSMAN = 208;
+    const BLESSED = 209;
+    const BERSERK  = 210;
+    const AXEMAN = 211;
+    const DEEPWOUNDS = 212;
+    const FAMINE = 213;
+    const ENRAGE = 214;
+    const LIGHTNING = 215;
+    const TRAP = 216;
+    const LUCKYCHARM = 217;
+    const LIGHTWEIGHT = 218;
+    const CRITICAL = 219;
+    const FASTTURN = 220;
+    const BLEED = 221;
+    
+
+    /**
+     * Sword
+     */
+
+    /**
+     * Bows
+     */
+    const AUTOAIM = 306;
+    const BLAZE = 311;
+    const BOMBARDMENT = 300;
+    const BOUNTYHUNTER = 309;
+    //const FIREWORK = 302;
+    const GRAPPLING = 313;
+    const HEADHUNTER = 312;
+    const HEALING = 310;
+    const HOMING = 316;
+    const MISSILE = 315;
+    const MOLOTOV = 304;
+    const PARALYZE = 303;
+    const PIERCING = 307;
+    const PORKIFIED = 314;
+    const SHUFFLE = 308;
+    const VOLLEY = 305;
+    const WITHERSKULL = 301;
+
+    /**
+     * Tools
+     */
+    const DRILLER = 206;
+    const ENERGIZING = 202;
+    const EXPLOSIVE = 200;
+    const HASTE = 207;
+    const JACKPOT = 212;
+    const OXYGENATE = 211;
+    const QUICKENING = 203;
+    const SMELTING = 201;
+    const TELEPATHY = 205;
+
+    /**
+     * Pickaxes
+     */
+
+    /**
+     * Axes
+     */
+    const LUMBERJACK = 204;
+
+    /**
+     * Shovels
+     */
+
+    /**
+     * Hoes
+     */
+    const FARMER = 209;
+    const FERTILIZER = 208;
+    const HARVEST = 210;
+
+    /**
+     * Armor
+     */
+    const INFERNO = 400;
+    const ENLIGHTED = 401;
+    const HARDENED = 402;
+    const ACIDBLOOD = 403;
+    const DODGE = 404;
+    const DAMAGELIMITER = 406;
+    const DEFLECT = 407;
+    const LASTSTAND = 408;
+    const PAINKILLER = 409;
+    const ARMORED = 410;
+    const TANK = 411;
+    const GODLYOVERLOAD = 412;
+    const OVERLOAD = 413;
+    const GROW = 414;
+    const SHRINK = 415;
+
+    /**
+     * Helmet
+     */
+    const GLOWING = 601;
+    const IMPLANTS = 600;
+    const MERMAID = 602;
+
+    /**
+     * Chestplate
+     */
+    const VOODOO = 801;
+
+    /**
+     * Leggings
+     */
+
+    /**
+     * Boots
+     */
+    const GEARS = 500;
+    const ANTIGRAVITY = 503;
+    const SYSTEMREBOOT = 504;
+    const SPRINGS = 501;
+    const ADRENLINE = 502;
+
+    /**
+     * Compass
+     */
+    const RADAR = 700;
+}
