ae730f2f94585bde3624edc34c9458252a523416
xSuper <62860563+VoltageMC@users.noreply.github.com>
2020-04-10 17:47:59 -0400

    Update CriticalEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/CriticalEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/CriticalEnchant.php
@@ -19,7 +19,7 @@ class CriticalEnchant extends ReactiveEnchantment
     /** @var string */
     public $name = "Critical";
     
-        /** @var int */
+    /** @var int */
     public $rarity = CustomEnchant::RARITY_EPIC;
     /** @var int */
     public $maxLevel = 2;
