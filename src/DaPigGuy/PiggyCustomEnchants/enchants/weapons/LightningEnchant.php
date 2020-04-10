781340d8102918df078de97ab25a3fb00986fa07
xSuper <62860563+VoltageMC@users.noreply.github.com>
2020-04-10 18:54:51 -0400

    Update LightningEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/LightningEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/LightningEnchant.php
@@ -6,6 +6,7 @@ namespace DaPigGuy\PiggyCustomEnchants\enchants\weapons;
 
 use DaPigGuy\PiggyCustomEnchants\enchants\CustomEnchant;
 use DaPigGuy\PiggyCustomEnchants\enchants\ReactiveEnchantment;
+use DaPigGuy\PiggyCustomEnchants\utils\Utils;
 use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Event;
