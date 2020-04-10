ab340d474cf4e0a6ab9ae4356a97f8c1e10043d3
xSuper <62860563+VoltageMC@users.noreply.github.com>
2020-04-10 18:53:09 -0400

    Update EnrageEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/EnrageEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/EnrageEnchant.php
@@ -6,6 +6,7 @@ namespace DaPigGuy\PiggyCustomEnchants\enchants\weapons;
 
 use DaPigGuy\PiggyCustomEnchants\enchants\CustomEnchant;
 use DaPigGuy\PiggyCustomEnchants\enchants\ReactiveEnchantment;
+use DaPigGuy\PiggyCustomEnchants\utils\Utils;
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
 use pocketmine\event\entity\EntityDamageEvent;
