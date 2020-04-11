f8d77a0402ceea1c45e218d615719ceb79dc7f0b
pleasepoggitwork <63435222+pleasepoggitwork@users.noreply.github.com>
2020-04-11 14:20:48 -0400

    Update SwordsmanEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/SwordsmanEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/SwordsmanEnchant.php
@@ -21,7 +21,7 @@ class SwordsmanEnchant extends ReactiveEnchantment
     public $name = "Swordsman";
     
     /** @var int */
-    public $rarity = CustomEnchant::RARITY_EPIC;
+    public $rarity = CustomEnchant::RARITY_RARE;
     /** @var int */
     public $maxLevel = 3;
     
