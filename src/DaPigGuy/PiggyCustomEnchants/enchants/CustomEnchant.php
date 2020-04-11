550d2c1863382f2a1fbaf3d63f0cc9d6db707a56
pleasepoggitwork <63435222+pleasepoggitwork@users.noreply.github.com>
2020-04-11 14:51:12 -0400

    Update CustomEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/CustomEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/CustomEnchant.php
@@ -62,7 +62,6 @@ class CustomEnchant extends Enchantment
     const ITEM_TYPE_LEGGINGS = 13;
     const ITEM_TYPE_BOOTS = 14;
     const ITEM_TYPE_COMPASS = 15;
-    const RARITY_RARE = "Rare";
 
     /**
      * @throws ReflectionException
