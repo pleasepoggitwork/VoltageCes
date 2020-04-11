d6be0f4f36018237d5de4db533e21f19f413a2a9
pleasepoggitwork <63435222+pleasepoggitwork@users.noreply.github.com>
2020-04-11 14:19:38 -0400

    Update FastTurnEnchant.php

--- src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/FastTurnEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/weapons/FastTurnEnchant.php
@@ -21,7 +21,7 @@ class FastTurnEnchant extends ReactiveEnchantment
     public $name = "FastTurn";
     
     /** @var int */
-    public $rarity = CustomEnchant::RARITY_EPIC;
+    public $rarity = CustomEnchant::RARITY_RARE;
     /** @var int */
     public $maxLevel = 2;
     
